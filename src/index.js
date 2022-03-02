4c42059b6df2ac04f277364ba918dc4e1082721e
derbenev <derbenev.vs@gmail.com>
2022-03-02 19:53:53 +0300

    lesson-2

--- src/index.js
+++ src/index.js
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM, { unstable_renderSubtreeIntoContainer } from "react-dom";
 import "./index.css";
 import { App } from "./App";
 
-const myTextTest = (
-  <p>Ghbdtn</p>
-  // <button onClick={clickText}>
-  //   Нажми на кнопку
-  // </button>
-);
-const myTextClick = "Всем привет!!!";
-const clickText = () => {
-  console.log("клик!");
+// const myTextTest = (
+//   <p>Ghbdtn</p>
+//   <button onClick={clickText}>
+//     Нажми на кнопку
+//   </button>
+// );
+
+// const myTextClick = "Всем привет!!!";
+// const clickText = () => {
+//   console.log("клик!");
+//   return (
+//     <div>
+//       <h1 className="App-header">{myTextClick}</h1>
+//     </div>
+//   );
+// };
+// const Message = () => {
+//   return (<button onClick={clickText}>Нажми на кнопку</button>);
+// };
+
+const MessageList = ({ onClick }) => {
+  console.log("ntcn");
+  const [messages, sendMessages] = useState(["massage 1"]);
+};
+
+const sendMessage = (message) => {
+  sendMessages([...messages, message]);
+}
+const UserText = () => {
   return (
     <div>
-      <h1 className="App-header">{myTextClick}</h1>
+      <button onClick={() => sendMessage("Test")}>Отправить сообщение</button>
+      <h2>message</h2>
     </div>
   );
-};
-const Message = () => {
-  return (<button onClick={clickText}>Нажми на кнопку</button>);
 };
 
 ReactDOM.render(
   <React.StrictMode>
+    <UserText />
     <App />
-    <Message />
-    
+    <MessageList />
   </React.StrictMode>,
   document.getElementById("root")
 );
